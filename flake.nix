be4796201f3e404298f03421f95261120930c9c2
Alexander Linder <alexander.linder@mondcarion.group>
2023-02-14 17:41:16 +0100

    Remove inputs

--- flake.nix
+++ flake.nix
@@ -2,10 +2,6 @@
   description = "Integrates sops into nixos";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, sops-nix }:{
